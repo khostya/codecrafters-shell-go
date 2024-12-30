90984c0cf682c21918200ecb441ed50b9d9b3bd3
khostya <khostya.konstantin@gmail.com>
2024-12-30 15:02:29 +0500

    3

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			continue
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }

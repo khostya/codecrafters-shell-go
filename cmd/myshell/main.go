1252632d3c528d35f9968d0b2ec0c6150c761ea4
khostya <khostya.konstantin@gmail.com>
2024-12-30 15:15:06 +0500

    4

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,9 +2,33 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func exitCMD(command []string) error {
+	if len(command) < 1 {
+		return errors.New("no command given")
+	}
+	i, err := strconv.Atoi(command[1])
+	if err != nil {
+		return err
+	}
+	os.Exit(i)
+	return nil
+}
+
+func eval(command []string) error {
+	switch command[0] {
+	case "exit":
+		return exitCMD(command)
+	default:
+		return errors.New("unknown command: " + command[0])
+	}
+}
 
 func main() {
 	for {
@@ -14,6 +38,10 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			continue
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
+
+		err = eval(strings.Split(command[:len(command)-1], " "))
+		if err != nil {
+			fmt.Println(command[:len(command)-1] + ": command not found")
+		}
 	}
 }

de96a2da67df2422e9147e810e27468260f0bbfa
root <root@khostya.>
2024-12-31 22:57:23 +0500

    codecrafters submit [skip ci]

--- internal/command/cmd_cd.go
+++ internal/command/cmd_cd.go
@@ -3,6 +3,7 @@ package command
 import (
 	"github.com/codecrafters-io/shell-starter-go/internal/model"
 	"os"
+	"strings"
 )
 
 func (c Commands) CdAbsolutePathCMD(command model.Command) model.Output {
@@ -14,5 +15,37 @@ func (c Commands) CdAbsolutePathCMD(command model.Command) model.Output {
 }
 
 func (c Commands) cdCMD(command model.Command) model.Output {
-	return c.CdAbsolutePathCMD(command)
+	if command.Args()[0] == "" {
+		return model.Output{}
+	}
+	if command.Args()[0][0] == '/' {
+		return c.CdAbsolutePathCMD(command)
+	}
+
+	out := c.pwdCMD(command)
+	if out.Stderr() != "" {
+		return out
+	}
+
+	stdout := strings.TrimRight(out.Stdout(), "\n")
+
+	stack := strings.Split(stdout, string(os.PathSeparator))
+
+	cd := strings.Split(command.Args()[0], string(os.PathSeparator))
+	for _, s := range cd {
+		if s == "" {
+			continue
+		}
+		if s == "." {
+			continue
+		}
+		if s == ".." {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s)
+		}
+	}
+
+	path := strings.Join(stack, string(os.PathSeparator))
+	return c.CdAbsolutePathCMD(model.NewCommand(cdCommand, path))
 }
